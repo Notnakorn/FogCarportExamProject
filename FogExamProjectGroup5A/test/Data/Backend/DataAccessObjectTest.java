235ea259e64e788cfcd03ab20b106b96b4900385
LennartBedrager <kvjonassen@gmail.com>
2017-05-07 16:33:25 +0200

    test changes

--- FogExamProjectGroup5A/test/Data/Backend/DataAccessObjectTest.java
+++ FogExamProjectGroup5A/test/Data/Backend/DataAccessObjectTest.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Data.Backend;
 
 
@@ -15,21 +11,6 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Anton
- */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/**
- *
- * @author Smaug
- */
 public class DataAccessObjectTest {
     
     DataAccessObject dao;
