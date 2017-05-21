b1e79a3cc47cbf6209f032f905be3735447b0008
LennartBedrager <kvjonassen@gmail.com>
2017-05-21 19:32:03 +0200

    Sum sum

--- FogExamProjectGroup5A/src/java/FrontEnd/FrontController/reviewCommand.java
+++ FogExamProjectGroup5A/src/java/FrontEnd/FrontController/reviewCommand.java
@@ -10,25 +10,42 @@ import FrontEnd.FrontController.Command;
 import FrontEnd.FrontController.CommandException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 
 
 class reviewCommand implements Command{
 
     private String next;
+    Controller ctrl;
     
     public reviewCommand(String jsp) {
         next  = jsp;
     }
 
+        
 
     @Override
     public String execute(HttpServletRequest request) throws CommandException {
+        
+        
+        try {
+            ctrl = new Controller();
+        } catch (Exception ex) {
+            Logger.getLogger(carportOrderCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         int review = 0;
         String r = (String) request.getParameter("smiley");
         review = Integer.parseInt(r);
-
+        
+        try {
+            ctrl.runReview(review);
+        } catch (Exception ex) {
+            Logger.getLogger(reviewCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         return next;
     }
     
